189219342cedb39662c63574f5c4130a5b1f6c7e
Sankar Battula <sankar.battula2@gmail.com>
2022-03-14 21:20:42 +0530

    Demo controllers

---
+++ demo-controllers/src/main/java/io/sb/democontrollers/DemoControllersApplication.java
@@ -0,0 +1,13 @@
+package io.sb.democontrollers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoControllersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoControllersApplication.class, args);
+    }
+
+}
---
+++ demo-controllers/src/test/java/io/sb/democontrollers/DemoControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sb.democontrollers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoControllersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
